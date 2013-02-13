f7c63e023eb31fb3715ec3b8ec0188b526e04340
awong <alan_kt_wong@yahoo.com>
2013-02-13 16:45:20 -0500

    Add pub sub plugin

---
+++ pubsub/jquery.pubsub.js
@@ -0,0 +1,206 @@
+/**
+ * <p>This is a fork of the publish/subscribe library by AmplifyJS.
+ * It has virtually the same signature and implementation EXCEPT,
+ * that the methods (publish,subscribe & unsubscribe) are directly
+ * bound to the jQuery object. This addresses the following use
+ * cases:</p>
+ * 
+ * <ul>
+ * <li>Subscribe and publish without data</li>
+ * <li>Subscribe and publish with data [majority case]</li>
+ * <li>Subscribe with a context and publish with data</li>
+ * <li>Subscribe to a topic with differing priorities, and publish with data</li>
+ * </ul>
+ * 
+ * <p>For examples of each of the following use cases, see the demo folder</p>
+ * 
+ * @author awong
+ * @see https://github.com/appendto/amplify/tree/master/core
+ * 
+ */
+;(function( $, undefined ) {
+	/*
+	 * Encapusulate state of pubsub event bus in following object.
+	 * The structure of the {@code subscriptions} cache will look like:
+	 * {  "/topic/1" : [
+	 *                   {priority: 10, callback: fn, context: null},
+	 *                   {priority: 12, callback: fn2, context: ctx}
+	 *                 ],
+	 *    "/topic/2" : [
+	 *                   {priority: 10, callback: fn3, context: ctx1},
+	 *                   {priority: 12, callback: fn4, context: ctx2}
+	 *                 ]
+	 * }
+	 */
+	var pubsub = {
+		slice : [].slice,
+		version : "0.0.1",
+		subscriptions : {}
+	};
+	
+	/**
+	 * Publish a message.
+	 * 
+	 * <ul>
+	 * <li>topic: Name of the message to subscribe to.</li>
+	 * <li>Any additional parameters will be passed to the subscriptions.</li>
+	 * </ul>
+	 * 
+	 * Example:
+	 *    $.subscribe( topic, callback )
+	 *    $.subscribe( topic, context, callback )
+	 *    $.subscribe( topic, callback, priority )
+	 *    $.subscribe( topic, context, callback, priority )
+	 * 
+	 * $.publish returns a boolean indicating whether any subscriptions returned false.
+	 * The return value is true if none of the subscriptions returned false, and false otherwise.
+	 * Note that only one subscription can return false because doing so will prevent additional
+	 * subscriptions from being invoked.
+	 * 
+	 */
+	pubsub.publish = function( topic ) {
+		if ( $.type(topic) !== "string" ) {
+			throw new Error( "You must provide a valid topic to publish." );
+		}
+
+		var args = pubsub.slice.call( arguments, 1 ),
+			topicSubscriptions,
+			subscription,
+			length,
+			i = 0,
+			ret;
+		
+		var registrations = pubsub.subscriptions[ topic ];
+
+		if ( !registrations ) {
+			return true;
+		}
+
+		topicSubscriptions = registrations.slice();
+		for ( length = topicSubscriptions.length; i < length; i++ ) {
+			subscription = topicSubscriptions[ i ];
+			var _cb = subscription.callback;
+			ret = _cb.apply( subscription.context, args );
+			if ( ret === false ) {
+				break;
+			}
+		}
+		return ret !== false;
+	};
+	/**
+	 * 
+	 * Subscribe to a message.
+	 * 
+	 * <ul>
+	 * <li>topic: Name of the message to subscribe to.</li>
+	 * <li>[context]: What this will be when the callback is invoked.</li>
+	 * <li>callback: Function to invoke when the message is published.</li>
+	 * <li>[priority]: Priority relative to other subscriptions for the same message. Lower values have higher priority. Default is 10.</li>
+	 * </ul>
+	 * 
+	 * Example:
+	 *    $.subscribe( topic, callback )
+	 *    $.subscribe( topic, context, callback )
+	 *    $.subscribe( topic, callback, priority )
+	 *    $.subscribe( topic, context, callback, priority )
+	 * 
+	 * Returning false from a subscription's callback will prevent any additional subscriptions
+	 * from being invoked and will cause amplify.publish to return false.
+	 * 
+	 */
+	pubsub.subscribe = function( topic /*string */, context /* object */, callback /* function */, priority /* integer */ ) {
+		if ( $.type(topic) !== "string" ) {
+			throw new Error( "You must provide a valid topic to create a subscription." );
+		}
+		// pubsub.subscribe( topic, callback, priority )
+		if ( arguments.length === 3 && $.type(callback) === "number" ) {
+			priority = callback;
+			callback = context;
+			context = null;
+		}
+		// pubsub.subscribe( topic, callback )
+		if ( arguments.length === 2 ) {
+			callback = context;
+			context = null;
+		}
+		priority = priority || 10;
+		if ( $.type(callback) !== "function" ) {
+			throw new Error( "You must provide a valid handle to the callback to add its subscription." );
+		}
+
+		var topicIndex = 0,
+			topics = topic.split( /\s/ ),
+			topicLength = topics.length,
+			added;
+		for ( ; topicIndex < topicLength; topicIndex++ ) {
+			topic = topics[ topicIndex ];
+			added = false;
+
+			if ( !pubsub.subscriptions[ topic ] ) {
+				pubsub.subscriptions[ topic ] = [];
+			}
+
+			var i = pubsub.subscriptions[ topic ].length - 1,
+				subscriptionInfo = {
+					callback: callback,
+					context: context,
+					priority: priority
+				};
+
+			for ( ; i >= 0; i-- ) {
+				if ( pubsub.subscriptions[ topic ][ i ].priority <= priority ) {
+					pubsub.subscriptions[ topic ].splice( i + 1, 0, subscriptionInfo );
+					added = true;
+					break;
+				}
+			}
+
+			if ( !added ) {
+				pubsub.subscriptions[ topic ].unshift( subscriptionInfo );
+			}
+		}
+
+		return callback;
+	};
+	/**
+	 * Remove a subscription.
+	 * 
+	 * <ul>
+	 * <li>topic: The topic being unsubscribed from.</li>
+	 * <li>callback: The callback that was originally subscribed.</li>
+	 * </ul>
+	 */
+	pubsub.unsubscribe =  function( topic /* string */, callback /* function */ ) {
+		if ( $.type(topic) !== "string" ) {
+			throw new Error( "You must provide a valid topic to remove a subscription." );
+		}
+		
+		var registrations = pubsub.subscriptions[ topic ];
+
+		if ( !registrations ) {
+			return;
+		}
+		
+		if ( !callback ) {
+			return registrations;
+		}
+		
+		if ( $.type(callback) !== "function" ) {
+			throw new Error( "You must provide a valid handle to the callback to remove its subscription." );
+		}
+
+		var length = registrations.length,
+			i = 0;
+
+		for ( ; i < length; i++ ) {
+			if ( registrations[ i ].callback === callback ) {
+				registrations.splice( i, 1 );
+				return registrations;
+			}
+		}
+	};
+	// now publicize the API on the pubsub object onto the jQuery object
+	$.publish = pubsub.publish;
+	$.subscribe = pubsub.subscribe;
+	$.unsubscribe = pubsub.unsubscribe;
+}( jQuery ) );
---
+++ pubsub/test/test.jquery.pubsub.js
@@ -0,0 +1,216 @@
+module( "jquery.pubsub testing" );
+
+test( "topic", function() {
+	expect( 5 );
+
+	try {
+		$.publish( undefined, function() {} );
+	} catch( err ) {
+		strictEqual( err.message, "You must provide a valid topic to publish.",
+			"error with no topic to publish" );
+	}
+
+	try {
+		$.subscribe( undefined, function() {} );
+	} catch( err ) {
+		strictEqual( err.message, "You must provide a valid topic to create a subscription.",
+			"error with no topic to subscribe" );
+	}
+	
+	try {
+		$.subscribe( "/fake/topic", "fooey callback" );
+	} catch( err ) {
+		strictEqual( err.message, "You must provide a valid handle to the callback to add its subscription.",
+			"error with no callback to subscribe" );
+	}
+
+	try {
+		$.unsubscribe( undefined, function() {} );
+	} catch( err ) {
+		strictEqual( err.message, "You must provide a valid topic to remove a subscription.",
+			"error with no topic to unsubscribe" );
+	}
+	try {
+		var f = $.subscribe( "/fake/topic", function() {} );
+		$.unsubscribe( "/fake/topic","fooey callback" );
+	} catch( err ) {
+		strictEqual( err.message, "You must provide a valid handle to the callback to remove its subscription.",
+			"error with no callback to unsubscribe" );
+	}
+});
+
+test( "continuation", function() {
+	expect( 7 );
+	$.subscribe( "continuation", function() {
+		ok( true, "first subscriber called" );
+	});
+	$.subscribe( "continuation", function() {
+		ok( true, "continued after no return value" );
+		return true;
+	});
+	strictEqual( $.publish( "continuation" ), true,
+		"return true when subscriptions are not stopped" );
+
+	$.subscribe( "continuation", function() {
+		ok( true, "continued after returning true" );
+		return false;
+	});
+	$.subscribe( "continuation", function() {
+		ok( false, "continued after returning false" );
+	});
+	strictEqual( $.publish( "continuation" ), false,
+		"return false when subscriptions are stopped" );
+});
+
+test( "priority", function() {
+	expect( 5 );
+	var order = 0;
+
+	$.subscribe( "priority", function() {
+		strictEqual( order, 1, "priority default; #1" );
+		order++;
+	});
+	$.subscribe( "priority", function() {
+		strictEqual( order, 3, "priority 15; #1" );
+		order++;
+	}, 15 );
+	$.subscribe( "priority", function() {
+		strictEqual( order, 2, "priority default; #2" );
+		order++;
+	});
+	$.subscribe( "priority", function() {
+		strictEqual( order, 0, "priority 1; #1" );
+		order++;
+	}, 1 );
+	$.subscribe( "priority", {}, function() {
+		strictEqual( order, 4, "priority 15; #2" );
+		order++;
+	}, 15 );
+	$.publish( "priority" );
+});
+
+test( "context", function() {
+	expect( 3 );
+	var obj = {},
+		fn = function() {};
+
+	$.subscribe( "context", function() {
+		strictEqual( this, window, "default context" );
+	});
+	$.subscribe( "context", obj, function() {
+		strictEqual( this, obj, "object" );
+	});
+	$.subscribe( "context", fn, function() {
+		strictEqual( this, fn, "function" );
+	});
+	$.publish( "context" );
+});
+
+test( "data", function() {
+	$.subscribe( "data", function( string, number, object ) {
+		strictEqual( string, "hello", "string passed" );
+		strictEqual( number, 5, "number passed" );
+		deepEqual( object, {
+			foo: "bar",
+			baz: "qux"
+		}, "object passed" );
+		string = "goodbye";
+		object.baz = "quux";
+	});
+	$.subscribe( "data", function( string, number, object ) {
+		strictEqual( string, "hello", "string unchanged" );
+		strictEqual( number, 5, "number unchanged" );
+		deepEqual( object, {
+			foo: "bar",
+			baz: "quux"
+		}, "object changed" );
+	});
+
+	var obj = {
+		foo: "bar",
+		baz: "qux"
+	};
+	$.publish( "data", "hello", 5, obj );
+	deepEqual( obj, {
+		foo: "bar",
+		baz: "quux"
+	}, "object updated" );
+});
+
+test( "unsubscribe", function() {
+	expect( 4 );
+	var order = 0;
+
+	$.subscribe( "unsubscribe", function() {
+		strictEqual( order, 0, "first subscriber called" );
+		order++;
+	});
+	var fn = function() {
+		ok( false, "removed by original reference" );
+		order++;
+	};
+	$.subscribe( "unsubscribe", fn );
+	$.subscribe( "unsubscribe", function() {
+		strictEqual( order, 1, "second subscriber called" );
+		order++;
+	});
+	var fn2 = $.subscribe( "unsubscribe", function() {
+		ok( false, "removed by returned reference" );
+		order++;
+	});
+	$.unsubscribe( "unsubscribe", fn );
+	$.unsubscribe( "unsubscribe", fn2 );
+	try {
+		$.unsubscribe( "unsubscribe", function() {});
+		ok( true, "no error with invalid handler" );
+	} catch ( e ) {
+		ok( false, "error with invalid handler" );
+	}
+	try {
+		$.unsubscribe( "unsubscribe2", function() {});
+		ok( true, "no error with invalid topic" );
+	} catch ( e ) {
+		ok( false, "error with invalid topic" );
+	}
+	$.publish( "unsubscribe" );
+});
+
+test( "unsubscribe during publish", function() {
+	expect( 3 );
+
+	function racer() {
+		ok( true, "second" );
+		$.unsubscribe( "racy", racer );
+	}
+
+	$.subscribe( "racy", function() {
+		ok( true, "first" );
+	});
+	$.subscribe( "racy", racer );
+	$.subscribe( "racy", function() {
+		ok( true, "third" );
+	});
+	$.publish( "racy" );
+});
+
+test( "multiple subscriptions", function() {
+	expect( 4 );
+
+	$.subscribe( "/sub/a/1 /sub/a/2 /sub/a/3", function() {
+		ok( true );
+	});
+	$.publish( "/sub/a/1" );
+
+	$.subscribe( "/sub/b/1 /sub/b/2", function() {
+		ok( true );
+	});
+	
+	// Test for Ticket #18
+	$.subscribe( "/sub/b/1 /sub/b/3", function() {
+		ok( true );
+	});
+	
+	$.publish( "/sub/b/2" );
+	$.publish( "/sub/b/2" );
+	$.publish( "/sub/b/3" );
+});
