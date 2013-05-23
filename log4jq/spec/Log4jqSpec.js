e972f1d77ab08c383e5b61b708d018801f880b2d
awong <alan_kt_wong@yahoo.com>
2013-05-23 12:54:42 -0400

    Add jasmine tests for log4jq

---
+++ log4jq/spec/Log4jqSpec.js
@@ -0,0 +1,259 @@
+describe("Log4jq", function() {
+
+	var testTarget = {
+		name: "testTarget",
+		version: "0.0.1",
+		subscribed: true,
+		/*
+		 * Publishes an entry as formatted string onto a topic
+		 * 
+		 * Parameters:
+		 *		entry -   The entry to log.
+		 */
+		log: function (entry) {
+			var _self = testTarget;
+			testTarget.set(entry);
+		},
+		set: function(entry) {
+			var $div = $(document);
+			$div.data("logger-test",entry);
+		},
+		get: function() {
+			var $div = $(document);
+			var _r = $div.data("logger-test");
+			return _r;
+		}
+	};
+
+	var configure = function(level) {
+		return $.configureLog4jq({
+			enabled: true,
+			level : level,
+			targets : [
+				{
+					name: "alert",
+					subscribed: false
+				},
+				{
+					name: "console",
+					subscribed: true
+				},
+				{
+					name: "divInsert",
+					subscribed: true,
+					$dom : $("div#console-log")
+				},
+				testTarget
+			]
+		});
+	};
+	
+	var logger;
+	
+	beforeEach(function() {
+		logger = configure("debug");
+	});
+
+	describe("when logger is configured", function() {
+		var activeSubscribers, alertTarget, consoleTarget, divTarget, customTarget;
+		var level = "debug"
+		
+		beforeEach(function() {
+			logger = configure(level);
+			
+			activeSubscribers = logger.subscribers();
+			
+			alertTarget   = logger.findTarget("alert");
+			consoleTarget = logger.findTarget("console");
+			divTarget     = logger.findTarget("divInsert");
+			customTarget  = logger.findTarget("testTarget");
+			
+		});
+
+		it("should enable the logger at DEBUG", function() {
+			expect(logger).toBeLoggingAt(level);
+		});
+		it("should set up 3 active log targets", function() {
+			expect(activeSubscribers.length).toBe(3);
+		});
+		it("should set up 3 active log targets in order", function() {
+			expect(alertTarget.subscribed).toBe(false);
+			expect(consoleTarget.subscribed).toBe(true);
+			expect(divTarget.subscribed).toBe(true);
+			expect(customTarget.subscribed).toBe(true);
+			expect(activeSubscribers[0]).toBe(consoleTarget);
+			expect(activeSubscribers[1]).toBe(divTarget);
+			expect(activeSubscribers[2]).toBe(customTarget);
+		});
+	});
+
+	describe("when logger is set to TRACE", function() {
+		var logEntry, testTarget, plan, logEntry;
+		var level = "trace";
+		
+		beforeEach(function() {
+			plan = { planId : "alan", foo : function() { var bar }, baz : "quus" }; 
+			logger = configure(level);
+			testTarget = logger.findTarget("testTarget");
+		});
+		
+		it("should enable the logger at TRACE", function() {
+			expect(logger).toBeLoggingAt(level);
+		});
+		
+		it("should trace a string message", function() {
+			var hello = "hello";
+			$.trace(hello);
+			expect(logger).toLogString(hello, level);
+		});
+		
+		it("should log an object with empty 'message' and full json", function() {
+			$.trace(plan);
+			logEntry = testTarget.get();
+			expect(logger).toLogJSON(plan, level);
+		});
+		it("should log an object and a string with full 'message' and full json", function() {
+			var hello = "hello";
+			$.trace(hello, plan);
+			expect(logger).toLogBoth(hello, plan, level);
+		});
+		it("should log with a context", function() {
+			expect(logger).toLogContext(level, $.trace);
+		});
+	});
+
+	describe("when logger is set to DEBUG", function() {
+		var logEntry, testTarget, plan, logEntry;
+		var level = "debug";
+		
+		beforeEach(function() {
+			plan = { planId : "alan", foo : function() { var bar }, baz : "quus" }; 
+			logger = configure(level);
+			testTarget = logger.findTarget("testTarget");
+		});
+		
+		it("should enable the logger at DEBUG", function() {
+			expect(logger).toBeLoggingAt(level);
+		});
+		
+		it("should trace a string message", function() {
+			var hello = "hello";
+			$.debug(hello);
+			expect(logger).toLogString(hello, level);
+		});
+		
+		it("should log an object with empty 'message' and full json", function() {
+			$.debug(plan);
+			expect(logger).toLogJSON(plan, level);
+		});
+		it("should log an object and a string with full 'message' and full json", function() {
+			var hello = "hello";
+			$.debug(hello, plan);
+			expect(logger).toLogBoth(hello, plan, level);
+		});
+		it("should log with a context", function() {
+			expect(logger).toLogContext(level, $.debug);
+		});
+	});
+	
+	describe("when logger is set to INFO", function() {
+		var logEntry, testTarget, plan, logEntry;
+		var level = "info";
+		
+		beforeEach(function() {
+			plan = { planId : "alan", foo : function() { var bar }, baz : "quus" }; 
+			logger = configure(level);
+			testTarget = logger.findTarget("testTarget");
+		});
+		
+		it("should enable the logger at INFO", function() {
+			expect(logger).toBeLoggingAt(level);
+		});
+		
+		it("should trace a string message", function() {
+			var hello = "hello";
+			$.info(hello);
+			expect(logger).toLogString(hello, level);
+		});
+		
+		it("should log an object with empty 'message' and full json", function() {
+			$.info(plan);
+			expect(logger).toLogJSON(plan, level);
+		});
+		it("should log an object and a string with full 'message' and full json", function() {
+			var hello = "hello";
+			$.info(hello, plan);
+			expect(logger).toLogBoth(hello, plan, level);
+		});
+		it("should log with a context", function() {
+			expect(logger).toLogContext(level, $.info);
+		});
+	});
+	
+	describe("when logger is set to WARN", function() {
+		var logEntry, testTarget, plan, logEntry;
+		var level = "warn";
+		
+		beforeEach(function() {
+			plan = { planId : "alan", foo : function() { var bar }, baz : "quus" }; 
+			logger = configure(level);
+			testTarget = logger.findTarget("testTarget");
+		});
+		
+		it("should enable the logger at WARN", function() {
+			expect(logger).toBeLoggingAt(level);
+		});
+		
+		it("should trace a string message", function() {
+			var hello = "hello";
+			$.warn(hello);
+			expect(logger).toLogString(hello, level);
+		});
+		
+		it("should log an object with empty 'message' and full json", function() {
+			$.warn(plan);
+			expect(logger).toLogJSON(plan, level);
+		});
+		it("should log an object and a string with full 'message' and full json", function() {
+			var hello = "hello";
+			$.warn(hello, plan);
+			expect(logger).toLogBoth(hello, plan, level);
+		});
+		it("should log with a context", function() {
+			expect(logger).toLogContext(level, $.warn);
+		});
+	});
+	describe("when logger is set to ERROR", function() {
+		var logEntry, testTarget, plan, logEntry;
+		var level = "error";
+		
+		beforeEach(function() {
+			plan = { planId : "alan", foo : function() { var bar }, baz : "quus" }; 
+			logger = configure(level);
+			testTarget = logger.findTarget("testTarget");
+		});
+		
+		it("should enable the logger at ERROR", function() {
+			expect(logger).toBeLoggingAt(level);
+		});
+		
+		it("should trace a string message", function() {
+			var hello = "hello";
+			$.error(hello);
+			expect(logger).toLogString(hello, level);
+		});
+		
+		it("should log an object with empty 'message' and full json", function() {
+			$.error(plan);
+			expect(logger).toLogJSON(plan, level);
+		});
+		it("should log an object and a string with full 'message' and full json", function() {
+			var hello = "hello";
+			$.error(hello, plan);
+			expect(logger).toLogBoth(hello, plan, level);
+		});
+		it("should log with a context", function() {
+			expect(logger).toLogContext(level, $.error);
+		});
+	});
+});
---
+++ log4jq/spec/SpecHelper.js
@@ -0,0 +1,84 @@
+beforeEach(function() {
+	this.addMatchers({
+		toBeLoggingAt: function(level) {
+			var result = false;
+			var logger = this.actual;
+			
+			var alertTarget   = logger.findTarget("alert");
+			var consoleTarget = logger.findTarget("console");
+			var divTarget     = logger.findTarget("divInsert");
+			var testTarget = logger.findTarget("testTarget");
+			
+			var isEnabled = logger.enabled();
+			var actualLevel = logger.level();
+			var targets = logger.subscribers();
+			
+			if (logger && isEnabled === true && targets.length === 3) {
+				if (testTarget.subscribed === true && alertTarget.subscribed === false && consoleTarget.subscribed === true && divTarget.subscribed === true) {
+					result = actualLevel === level;
+				}
+			};
+			
+			return result;
+		},
+		toLogString: function(string,level) {
+			var result = false;
+			var logger = this.actual;
+			var testTarget = logger.findTarget("testTarget");
+			var logEntry = testTarget.get();
+			
+			if (logger && testTarget && logEntry) {
+				result = (logEntry.message === string && logEntry.timestamp instanceof Date && logEntry.getLevel() === level);
+			}
+			return result;
+		},
+		toLogJSON: function(json,level) {
+			var result = false;
+			var logger = this.actual;
+			var testTarget = logger.findTarget("testTarget");
+			var logEntry = testTarget.get();
+			
+			if (logger && testTarget && logEntry) {
+				result = (logEntry.timestamp instanceof Date && logEntry.getLevel() === level && _.isEqual(logEntry.json, json));
+			}
+			return result;
+		},
+		toLogBoth: function(string,json,level) {
+			var result = false;
+			var logger = this.actual;
+			var testTarget = logger.findTarget("testTarget");
+			var logEntry = testTarget.get();
+			
+			if (logger && testTarget && logEntry) {
+				var isString = logEntry.message === string && logEntry.timestamp instanceof Date && logEntry.getLevel() === level;
+				var isJSON   = logEntry.timestamp instanceof Date && logEntry.getLevel() === level && _.isEqual(logEntry.json, json);
+				result = (isString && isJSON);
+			}
+			return result;
+		},
+		toLogContext: function(level, logFunction) {
+			var result = false;
+			var logger = this.actual;
+
+			var moduleSvc = {
+					name : "moduleSvc",
+					message : level.toUpperCase() + " message",
+					execute : function() {
+						logFunction(moduleSvc.message, {}, moduleSvc );
+						var testTarget = logger.findTarget("testTarget");
+						var logEntry   = testTarget.get();
+						var logMessage = logEntry.format();
+						
+						var isLevel = logMessage.contains("[" + level.toUpperCase() +"]");
+						var isName = logMessage.contains(moduleSvc.name);
+						var isMessage = logMessage.contains(moduleSvc.message);
+						return isLevel && isName && isMessage;
+					}
+			};
+			if (logger && moduleSvc) {
+				result = moduleSvc.execute();
+			}
+			return result;
+		}
+	});
+});
