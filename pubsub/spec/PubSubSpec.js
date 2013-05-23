68a297baf8fbe50d2f8ac6df38024a91b0dbe29b
awong <alan_kt_wong@yahoo.com>
2013-05-23 16:19:51 -0400

    migrate jquery.pubsub qunit tests to jasmine

--- pubsub/spec/PubSubSpec.js
+++ pubsub/spec/PubSubSpec.js
@@ -185,4 +185,244 @@ describe("jquery.pubsub", function() {
 			bound("bound function");
 		});
 	});
+	
+	describe("when creating publications", function() {
+		var PubSub, publication, notification;
+		var topic = "/app/module/class";
+		var data = { id : 1, date: new Date(), name: "name" };
+		var context = {};
+		
+		beforeEach(function() {
+			PubSub = TestUtil.resetPubSub();
+		});
+		
+		it("should create them with data and context", function() {
+			publication = PubSub.createPublication(topic ,{ data: data, context: context });
+			notification = publication.notification;
+			expect(notification !== null).toBe(true);
+			expect(notification.data).toBe(data);
+			expect(notification.publishTopic).toBe(topic);
+			expect(notification.context).toBe(context);
+			expect(notification.timestamp !== null).toBe(true);
+			expect(publication.immediateExceptions).toBe(true);
+		});
+		it("should create them with just data", function() {
+			publication = PubSub.createPublication(topic, { data : data });
+			notification = publication.notification;
+			expect(notification.context === null).toBe(true);
+		});
+		it("should not create them with w/o options", function() {
+			try {
+				publication = PubSub.createPublication(topic)
+			} catch( err ) {
+				expect(err.message).toBe("You must provide options to create a Notification.");
+			}
+		});
+		it("should create them with empty options", function() {
+			publication = PubSub.createPublication(topic, {});
+			notification = publication.notification;
+			expect(notification.data === null).toBe(true);
+			expect(notification.context === null).toBe(true);
+		});
+	});
+	describe("when creating subscriptions", function() {
+		var PubSub, subscription, topics;
+		var topic = "/app/module/class";
+		var data = { id : 1, date: new Date(), name: "name" };
+		var context = {};
+		
+
+		var callback = function(notification) {
+			$.noop();
+		};
+		var priority = 100;
+		
+		beforeEach(function() {
+			PubSub = TestUtil.resetPubSub();
+			topics = PubSub.createTopics(topic);
+		});
+		
+		it("should create them with topic, callback, priority and context", function() {
+			var subscription = PubSub.createSubscription(topic, callback, priority, context);
+			expect(subscription !== null).toBe(true);
+			expect(subscription.timestamp !== null).toBe(true);
+			expect(_.isEqual(subscription.topics,topics)).toBe(true);
+			expect(subscription.callback).toBe(callback);
+			expect(subscription.priority).toBe(priority);
+			expect(subscription.context).toBe(context);
+		});
+		it("should create them with topic, callback and priority", function() {
+			var subscription = PubSub.createSubscription(topic, callback, priority);
+			expect(subscription.context === null).toBe(true);
+		});
+		it("should create them with topic and callback", function() {
+			var subscription = PubSub.createSubscription(topic, callback);
+			expect(subscription.context === null).toBe(true);
+			expect(subscription.priority).toBe(10);
+			// but this is not the 'official' way to subscribe
+			expect(PubSub.hasSubscriptions(topic)).toBe(false);
+		});
+	});
+	
+	describe("when handling errors for public API", function() {
+		var PubSub;
+		
+
+		var callback = function(notification) {
+			$.noop();
+		};
+		
+		beforeEach(function() {
+			PubSub = TestUtil.resetPubSub();
+		});
+		
+		it("should throw an error on publishing to bad topic name", function() {
+			try {
+				$.publish( undefined, callback );
+			} catch( err ) {
+				expect(err.message).toBe("You must provide a valid topic name to publish.");
+			}
+		});
+		it("should throw an error on subscribing with bad topic name", function() {
+			try {
+				$.subscribe( undefined, callback );
+			} catch( err ) {
+				expect(err.message).toBe("You must provide a valid topic name to create a Subscription.");
+			}
+		});
+		it("should throw an error on subscribing w/o a callback", function() {
+			try {
+				$.subscribe( "/fake/topic", "fooey callback" );
+			} catch( err ) {
+				expect(err.message).toBe("You must provide a valid handle to the callback to add its subscription.");
+			}
+		});
+		it("should throw an error on unsubscribing with a bad topic name", function() {
+			try {
+				$.unsubscribe( undefined );
+			} catch( err ) {
+				expect(err.message).toBe("You must provide a valid topic to remove a subscription.");
+			}
+			try {
+				$.unsubscribe( "bad name" );
+			} catch( err ) {
+				expect(err.message).toBe("You must provide a valid topic to remove a subscription.");
+			}
+		});
+	});
+	
+	describe("when subscribing", function() {
+		var PubSub, topic, topics;
+		
+		beforeEach(function() {
+			topic = "/app/module/class";
+			PubSub = TestUtil.resetPubSub();
+			topics = PubSub.createTopics(topic);
+		});
+		
+		it("should subscribe correctly to a topic with just a callback", function() {
+			var count = 0;
+			
+			var callbacks = {
+				first: {
+					notify : function(notification) {
+						expect(callbacks.first).toHaveBeenCalled();
+						$.debug("1st subscriber notified");
+						expect(count).toBe(0);
+						count++;
+					}
+				},
+				second: {
+					notify : function(notification) {
+						expect(callbacks.second).toHaveBeenCalled();
+						$.debug("1st subscriber notified");
+						expect(count).toBe(1);
+						count++;
+					}
+				}
+			};
+			spyOn(callbacks.first, 'notify');
+			spyOn(callbacks.second, 'notify');
+			
+			callbacks.first.subscription = $.subscribe(topic, callbacks.first.notify);
+			expect(PubSub.hasSubscriptions(topic)).toBe(true);
+			expect(PubSub.getSubscriptions(topic).length).toBe(1);
+			expect(callbacks.first.subscription).toBeSubscribedCorrectly(topic, null, callbacks.first.notify, null, topics);
+
+
+			callbacks.second.subscription = $.subscribe(topic, callbacks.second.notify);
+			expect(callbacks.second.subscription).toBeSubscribedCorrectly(topic, null, callbacks.second.notify, null, topics);
+			
+			expect(PubSub.getSubscriptions(topic).length).toBe(2);
+			// should have unique GUIDs
+			expect(callbacks.first.subscription.id).not.toBe(callbacks.second.subscription.id);
+			expect(callbacks.first.subscription.priority).toBe(callbacks.second.subscription.priority);
+			expect(callbacks.second.subscription.timestamp >= callbacks.first.subscription.timestamp).toBe(true);
+			
+			$.publishSync(topic);
+		});
+		it("should subscribe correctly to a topic with a context", function() {
+			var callbacks = {
+					first: {
+						notify : function(notification) {
+							expect(callbacks.first).toHaveBeenCalled();
+							$.debug("1st subscriber notified");
+							expect(count).toBe(0);
+						},
+						context : {}
+					}
+			};
+			spyOn(callbacks.first, 'notify');
+			callbacks.first.subscription = $.subscribe(topic, callbacks.first.context, callbacks.first.notify);
+
+			expect(PubSub.hasSubscriptions(topic)).toBe(true);
+			expect(PubSub.getSubscriptions(topic).length).toBe(1);
+			expect(callbacks.first.subscription).toBeSubscribedCorrectly(topic, callbacks.first.context, callbacks.first.notify, null, topics);
+
+			$.publishSync(topic);
+		});
+		
+		it("should subscribe correctly to topic with a callback and a priority", function() {
+			var callbacks = {
+					first: {
+						notify : function(notification) {
+							expect(callbacks.first).toHaveBeenCalled();
+							$.debug("1st subscriber notified");
+							expect(count).toBe(0);
+						},
+						priority : 100
+					}
+			};
+			spyOn(callbacks.first, 'notify');
+			callbacks.first.subscription = $.subscribe(topic, callbacks.first.notify, callbacks.first.priority);
+
+			expect(PubSub.hasSubscriptions(topic)).toBe(true);
+			expect(PubSub.getSubscriptions(topic).length).toBe(1);
+			expect(callbacks.first.subscription).toBeSubscribedCorrectly(topic, null, callbacks.first.notify, callbacks.first.priority, topics);
+
+			$.publishSync(topic);
+		});
+		it("should subscribe correctly to a topic with a callback, a priority and a context", function() {
+			var callbacks = {
+					first: {
+						notify : function(notification) {
+							expect(callbacks.first).toHaveBeenCalled();
+							$.debug("1st subscriber notified");
+							expect(count).toBe(0);
+						},
+						priority : 100,
+						context: {}
+					}
+			};
+			
+			spyOn(callbacks.first, 'notify');
+			callbacks.first.subscription = $.subscribe(topic, callbacks.first.context, callbacks.first.notify, callbacks.first.priority);
+			expect(PubSub.hasSubscriptions(topic)).toBe(true);
+			expect(PubSub.getSubscriptions(topic).length).toBe(1);
+			expect(callbacks.first.subscription).toBeSubscribedCorrectly(topic,callbacks.first.context, callbacks.first.notify, callbacks.first.priority, topics);
+			
+			$.publishSync(topic);
+		});
+	});
+
 });
--- pubsub/spec/SpecHelper.js
+++ pubsub/spec/SpecHelper.js
@@ -1,8 +1,30 @@
 beforeEach(function() {
 	this.addMatchers({
-		toBeTrivial: function(level) {
+		toBeSubscribedCorrectly: function(topic, context, callback, priority, topics) {
 			var result = false;
-			var PubSub = this.actual;
+			var subscription = this.actual;
+			var defaultPriority = 10;
+			if (subscription && subscription !== null) {
+				result = subscription.callback === callback
+					&& _.isEqual(subscription.topics,topics);
+				if (context !== null && priority !== null) {
+					result = result 
+						&& subscription.priority === priority
+						&& subscription.context === context;
+				} else if (context !== null) {
+					result = result 
+						&& subscription.priority === defaultPriority
+						&& subscription.context === context;
+				} else if (priority !== null) {
+					result = result 
+						&& subscription.priority === priority
+						&& subscription.context === null;
+				} else {
+					result = result 
+						&& subscription.priority === 10
+						&& subscription.context === null;
+				}
+			}
 			return result;
 		}
 	});
